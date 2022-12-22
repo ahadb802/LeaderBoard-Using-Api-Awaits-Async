0aebc2f67427389f9157a2fdbe59bbd54df4d1c7
ahadb802 <abdulahadb802@gmail.com>
2022-12-22 19:49:56 +0500

    fix refresh btn

--- src/modules/addScoreToForm.js
+++ src/modules/addScoreToForm.js
@@ -3,20 +3,23 @@ import renderScore from './renderScore.js';
 const addScoreToForm = async () => {
   const name = document.querySelector('#name').value;
   const score = document.querySelector('#score').value;
-  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1222/scores/', {
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/TNm1CNGjVKvWjBG9X4u9/scores/', {
     method: 'POST',
+
     body: JSON.stringify({ user: name, score }),
+
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
   });
-
+  document.querySelector('#name').value = '';
+  document.querySelector('#score').value = '';
   renderScore();
 };
 
 const addScore = () => {
   const form = document.querySelector('#score-btn');
-  form.addEventListener('submit', (e) => {
+  form.addEventListener('click', (e) => {
     e.preventDefault();
     addScoreToForm();
     e.target.reset();
--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,7 +1,7 @@
 import renderScore from './renderScore.js';
 
 const getData = async () => {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1222/scores', {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/TNm1CNGjVKvWjBG9X4u9/scores/', {
     method: 'GET',
   });
   const data = response.json();
